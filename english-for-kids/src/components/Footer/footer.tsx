f34ef07b6be715334a69ce406388b9ceb5841105
zhenuua <zhenuua@gmail.com>
2021-07-07 01:12:19 +0300

    feat: add footer

---
+++ english-for-kids/src/components/Footer/footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import './footer.scss';
+
+export default class Footer extends React.Component {
+ 
+  render(){
+    return (
+      <section className='footer-container'>
+        <a className="github" href="github.com/zhenuua" target="_blank" rel="noopener noreferrer">github</a>
+        {/* <p>2021 © English for Kids</p> */}
+        <a className="rss" href="rs.school/js/" target="_blank" rel="noopener">
+        <span className="rss-year">'21</span>
+        </a>
+      </section>
+    )
+  }
+}
