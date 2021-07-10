3b1c00a825c8d4faf26d10ed1cdc2ffa3a021a7f
zhenuua <zhenuua@gmail.com>
2021-07-10 18:48:31 +0300

    feat: add play mode with souds

--- english-for-kids/src/components/MainPage/Card.tsx
+++ english-for-kids/src/components/MainPage/Card.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 interface MyProps {
@@ -7,19 +8,33 @@ interface MyProps {
   translation?: string,
   audioSrc?: string,
   activeGameMode?: string,
+  compaireCurrentSound: (sound: string) => boolean,
+  nameButtonGame: string
 }
 interface MyState {
   playMode: boolean,
-  isFlipped: boolean
+  isFlipped: boolean,
+  disabled: boolean
 }
 
 export default class Card extends React.Component<MyProps, MyState> {
 
-  state = { playMode: false, isFlipped: false }
+  state = { 
+    playMode: false,
+    isFlipped: false,
+    disabled: false
+   }
 
   clickCard(title: string) {
-    if (this.props.audioSrc && this.props.activeGameMode==='train') {
-      this.playAudio(this.props.audioSrc)
+    
+    if (this.props.audioSrc) {
+      if (this.props.activeGameMode === 'play' && this.props.nameButtonGame === 'Repeat Word'){
+        if (this.props.compaireCurrentSound(this.props.audioSrc)){
+          this.setState({disabled : true})
+        }
+      } else if (this.props.activeGameMode === 'train'){
+        this.playAudio(this.props.audioSrc)
+      }
     } else {
       this.props.changeCategory(title);
     }
@@ -32,15 +47,17 @@ export default class Card extends React.Component<MyProps, MyState> {
     audio.play();
   }
 
+
   render() {
 
     let flippedButton = <div></div>
     let languageTittle = this.props.title
     let displayNone = '';
-    let cardModePlay ='';
+    let cardModePlay = '';
+
 
     if (this.props.audioSrc) {
-      flippedButton = <img className={`flipped-button ${this.state.isFlipped || this.props.activeGameMode==='play' ? 'display-none' : ''}`}
+      flippedButton = <img className={`flipped-button ${this.state.isFlipped || this.props.activeGameMode === 'play' ? 'display-none' : ''}`}
         onClick={() => {
           this.setState({ isFlipped: true })
         }}
@@ -50,23 +67,29 @@ export default class Card extends React.Component<MyProps, MyState> {
     if (this.state.isFlipped && this.props.translation) {
       languageTittle = this.props.translation
     }
-    
-    if (this.props.activeGameMode==='play' && this.props.translation){
+
+    if (this.props.activeGameMode === 'play' && this.props.translation) {
       cardModePlay = 'card__image__play'
       displayNone = 'display-none';
-    } else{
+    } else {
       displayNone = '';
-      cardModePlay='';
+      cardModePlay = '';
     }
 
     return (
-      <div className={`card-container ${this.state.isFlipped ? 'flipped' : ''}`}
-        onMouseLeave={(() => { this.setState({ isFlipped: false }) })}
+      <div className=
+      {`card-container ${this.state.isFlipped ? 'flipped' : '' } ${this.state.disabled ? 'card__disable': ''}  ` }
+
+        onMouseLeave={(() => {
+          if (this.state.isFlipped === true) {
+            this.setState({ isFlipped: false })
+          }
+        })}
         onClick={() => this.clickCard(this.props.title)}>
         <div className="card">
           <img className={`${cardModePlay} card__image ${this.state.isFlipped ? 'card__image__flipped' : ''}`}
             src={this.props.imgSrc} />
-          <hr className={`${displayNone} card__${this.props.activeGameMode==='train' ? 'hr-train' : 'hr-play'}`} />
+          <hr className={`${displayNone} card__${this.props.activeGameMode === 'train' ? 'hr-train' : 'hr-play'}`} />
           <p className={`${displayNone} card__name ${this.state.isFlipped ? 'scale' : ''}`}>{languageTittle}</p>
           {flippedButton}
         </div>
